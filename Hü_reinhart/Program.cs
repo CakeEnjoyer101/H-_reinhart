1db91dd952142a6c1fe01721052c1ab80f0a7c66
CakeEnjoyer101 <reinhart.s21@htlwienwest.at>
2024-01-14 19:22:16 +0100

    Projektdateien hinzufügen.

---
+++ Hü_reinhart/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hü_reinhart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
